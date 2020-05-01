943051c6036ae5f13f12bb068f5f76b6ae13f3ed
Joshua-Luke Bevan <joshua.bevan@madetech.com>
2020-05-01 15:13:41 +0100

    Added another test case for the number 5

--- romanNumerals.test.js
+++ romanNumerals.test.js
@@ -1,11 +1,15 @@
 romanNumerals = (numberToConvert) => {
-  if (numberToConvert <= 3 && numberToConvert !== 0) {
-    return "I".repeat(numberToConvert);
-  } else if (numberToConvert === 4) {
-    return "IV";
-  } else {
-    return "II";
+  let result = "";
+  if (numberToConvert) {
+    if (numberToConvert <= 3) {
+      result = "I".repeat(numberToConvert);
+    } else if (numberToConvert === 4) {
+      result = "IV";
+    } else {
+      result = "V";
+    }
   }
+  return result;
 };
 
 test("Write a passing test", () => {
@@ -14,7 +18,8 @@ test("Write a passing test", () => {
 
 test("Write a passing test for the right reasons", () => {
   // This passes because there is a function called romanNumerals
-  expect(romanNumerals()).toBe("II");
+  // This test case is giving eroneous results, refactoring
+  expect(romanNumerals()).toBe("");
 });
 
 test("Given the number '1', it should return 'I'", () => {
@@ -35,4 +40,9 @@ test("Given the number '3', it should return 'III", () => {
 test("Given the number '4', it should return 'IV'", () => {
   // This is a unique case for now, so we can slime it
   expect(romanNumerals(4)).toBe("IV");
+});
+
+test("Given the number '5', it should return 'V'", () => {
+  // Another unique case for now, so we can slime it
+  expect(romanNumerals(5)).toBe("V");
 });
