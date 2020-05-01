ad37c59668bcb69311ffd5b7ed1d503e4271e5b6
Joshua-Luke Bevan <joshua.bevan@madetech.com>
2020-05-01 13:59:38 +0100

    Added a failing test for the right reason

--- romanNumerals.test.js
+++ romanNumerals.test.js
@@ -1,3 +1,8 @@
 test("Write a failing test", () => {
   expect(true).toBe(false);
 });
+
+test("Write a failing test for the right reasons", () => {
+  // This fails because there is no function called romanNumerals
+  expect(romanNumerals(2)).toBe("I");
+});
